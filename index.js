9ce74ef48bda1a8d256466b640887b339dbde87e
George Russell <238458861+devgeorgerm-prog@users.noreply.github.com>
2025-10-18 18:41:45 +0800

    Creating server and logging

--- index.js
+++ index.js
@@ -1,39 +1,91 @@
 const express = require('express')
+var morgan = require('morgan')
 const app = express()
 
-let notes = [
+app.use(express.json())
+morgan.token('body', (req) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time[4] ms :body'))
+
+let persons = [
     {
-        id: "1",
-        content: "HTML is easy",
-        important: true
+        "id": "1",
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
     {
-        id: "2",
-        content: "Browser can execute only JavaScript",
-        important: false
+        "id": "2",
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
     {
-        id: "3",
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
+        "id": "3",
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": "4",
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
 ]
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello world</h1>')
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
 })
 
-app.get('/api/notes', (request, response) => {
-    response.json(notes)
+app.get('/info', (req, res) => {
+    const now = new Date();
+    res.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${now}</p>    
+    `)
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-    response.json(note)
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    const person = persons.find(person => person.id === id)
+    if (person) return res.json(person)
+    else res.status(404).end()
 })
 
-const PORT = 3001
+app.delete('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
+})
+
+const generateId = () => {
+    return String(Math.floor(Math.random() * 100))
+}
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+    const findDuplicate = persons.find(person => person.name === body.name)
+    if (!body.name || body.name === "") {
+        return res.status(400).json({
+            error: 'No name'
+        })
+    } else if (!body.number) {
+        return res.status(400).json({
+            error: 'No Number'
+        })
+    } else if (findDuplicate) {
+        return res.status(400).json({
+            error: 'Name must be unique'
+        })
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+    res.json(person)
+})
+
+const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+    console.log(`Server is running in ${PORT}`)
 })
